a5378c33017b89535e45c14ed842e635b63059f7
Hassan Lamidi <you@example.com>
2022-08-12 18:52:54 +0100

    updated the logo animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,69 @@
 import './index.scss'
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from '../Logo'
+
+
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobTitleArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
   return (
     <>
       <div className="container home-page">
         <div className="text-area">
           <h1>
-            Hello, <br />
-            I'm
-            <img src={LogoTitle} alt="developer" />
-            leekmaster
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
             <br />
-            Web Developer
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={10}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobTitleArray}
+              idx={22}
+            />
           </h1>
-          <div> 
-          <h2>FullStack Developer | ReactDeveloper | NodejsDeveloper</h2>
+          <div>
+            <h2>FullStack Developer | ReactDeveloper | NodejsDeveloper</h2>
           </div>
+          <Link to="/contact" className="contact-button">
+            CONTACT ME
+          </Link>
         </div>
-        <Link to="/contact" className="contact-button">
-          CONTACT ME
-        </Link>
+      <Logo />
       </div>
     </>
   )
